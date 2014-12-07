a93ddc8edb10e43987f3de60764ad94c747f73d3
Amber York <adyork@whoi.edu>
2014-12-07 18:47:02 -0500

    pasted getData.R into each plot R source file

--- plot1.R
+++ plot1.R
@@ -1,5 +1,9 @@
 if (!exists('powerData') ) {
-  source('getData.R')
+  zipfile<-('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip')
+  download.file(zipfile,"powerConsumption.zip")
+  unzip("powerConsumption.zip")
+  powerData<-read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?",stringsAsFactors=FALSE)
+  powerData<-powerData[as.Date(powerData$Date,format="%d/%m/%Y") >=  as.Date('2007-02-01') & as.Date(powerData$Date,format="%d/%m/%Y") <=  as.Date('2007-02-02'),]
 }
 png("plot1.png", width=480, height=480)
 hist(powerData$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
--- plot3.R
+++ plot3.R
@@ -1,5 +1,9 @@
 if (!exists('powerData') ) {
-  source('getData.R')
+  zipfile<-('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip')
+  download.file(zipfile,"powerConsumption.zip")
+  unzip("powerConsumption.zip")
+  powerData<-read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?",stringsAsFactors=FALSE)
+  powerData<-powerData[as.Date(powerData$Date,format="%d/%m/%Y") >=  as.Date('2007-02-01') & as.Date(powerData$Date,format="%d/%m/%Y") <=  as.Date('2007-02-02'),]
 }
 powerData$Timestamp<- strptime(paste(powerData$Date,powerData$Time),format="%d/%m/%Y %H:%M:%S",tz="utc")
 
